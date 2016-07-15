993b86825bbd801e11ad58530b6dfcc59965618d
John L Michaloski <michalos@rufous.el.nist.gov>
2016-07-15 14:37:24 -0400

    raw crcl asio package

--- rawcrcl/src/rawcrcl.cpp
+++ rawcrcl/src/rawcrcl.cpp
@@ -29,7 +29,7 @@ port: 47548
 
 
 */
-#include "nistcrcl.h"
+#include "rawcrcl.h"
 
 #include <boost/format.hpp>
 #include <string>
@@ -38,103 +38,121 @@ port: 47548
 #include <iostream>
 #include <sstream>
 
-#include <xercesc/dom/DOM.hpp>
-#include <xercesc/util/PlatformUtils.hpp>
-#include <xercesc/framework/XMLGrammarPoolImpl.hpp>
-
-
 #include <ros/ros.h>
 #include <ros/package.h>
 #include "std_msgs/String.h"
 // THis is part of this package
-#include "nistcrcl/CrclCommandMsg.h"
+#include "rawcrcl/CrclRawCommandMsg.h"
 
 
 #include "AsioCrclServer.h"
 #include "Globals.h"
 #include "Setup.h"
-#include "Crcl2Ros.h"
 
 ALogger Logger;
+static ros::Publisher crcl_pub; /**< ros publisher information used for crcl command updates */
+static ros::Subscriber crcl_sub ;
 
 
 // myios declared as static in AsioCrclServer.h
 CAsioCrclServer crclServer(myios);
 
-//static void crclstatusCallback(const std_msgs::String::ConstPtr& msg)
-//{
-//    ROS_INFO("Crcl status: [%s]", msg->data.c_str());
-//    //crclServer.SyncWrite(msg->data);
-//}
+CAsioCrclSession* pSession=NULL;
+
+// This is very kludgy and assumes only 1 subscriber wants feedback
+static void crclstatusCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("Crcl status: [%s]", msg->data.c_str());
+  if(pSession!=NULL)
+     pSession->SyncWrite(msg->data);
+}
 
 
 int main(int argc, char** argv) {
     
-
-    try {
-        // Initialize xercesc used by code synthesis to parse CRCL XML
-        xercesc::XMLPlatformUtils::Initialize();
+      
+        try {
         
         // Find path of executable
         std::string path(argv[0]);
         Globals.ExeDirectory = path.substr(0, path.find_last_of('/') + 1);
         Globals._appproperties["ExeDirectory"] = Globals.ExeDirectory;
-
+                
 #if 0
-        // This sets up some application name/value pairs: user, hostname
-        //SetupRosEnvironment - needs to go before ROS!
+       // This sets up some application name/value pairs: user, hostname
+ 		//SetupRosEnvironment - needs to go before ROS!
         SetupRosEnvironment("");
 #endif
         
         // Initialize ROS
         ros::init(argc, argv, "nistcrcl");
         ros::NodeHandle nh;
-        ros::Rate r(50);  // 50 times a second
+        ros::Rate r(50);  // 10 times a second - 10Hz
 
         //  Required for multithreaded ROS communication  NOT TRUE: if not ros::spinOnce
         ros::AsyncSpinner spinner(1);
         spinner.start();
-
+ 
         // ROS config - parameter list - save for comparison later
         std::string params = ReadRosParams(nh);
         Globals.WriteFile(Globals.ExeDirectory + "rosconfig.txt", params);
 
 
 #if 0
-        // missing getPath while linking
+	// missing getPath while linking
         path = ros::package::getPath("nistcrcl");
         Globals._appproperties["nistcrcl"] = path;
 #endif
 
 
-        // Logger
+       // Logger
         LogFile.Open(Globals.ExeDirectory + "logfile.log");
         LogFile.DebugLevel() = 5;
         LogFile.Timestamping() = true;
-
+  
         // CRCl Communication handler - bundles xml into messages
-        CAsioCrclServer::_bTrace = false;
+         CAsioCrclServer::_bTrace = false;
         
         // This initializes the asio crcl socket listener, in theory N clients can connect. Only 1 tested.
-        std::string crclip;
-        int crclport=64444;
-        nh.param<std::string>("crclip", crclip, "127.0.0.1");
-        nh.param("crclport", crclport, 64444);
-        ROS_INFO("Crcl listen: [%s:%d]", crclip.c_str(), crclport);
+	std::string crclip;
+	int crclport=64444;
+	nh.param<std::string>("crclip", crclip, "127.0.0.1");
+	nh.param("crclport", crclport, 64444);
+	ROS_INFO("Crcl listen: [%s:%d]", crclip.c_str(), crclport);
+        //crclServer.Init("127.0.0.1", 64444, "Command GUI");
         crclServer.Init(crclip, crclport, "Command GUI");
 
         CAsioCrclServer::_bTrace=true;
         crclServer.Start();
 
-        // This reads new Crcl XML message, interprets them, and translates into ROS
-        // Second, it publishes the Crcl into ROS onto nistcrcl/crcl_command topic
-        CCrcl2RosMsg crcl2ros(nh,10.0);
-        crcl2ros.Start();
+       
+	//crcl_pub = nh.advertise<std_msgs::String>("crcl_command", 10);
+	//crcl_sub = nh.subscribe("crcl_status", 1000, crclstatusCallback);
+
+	crcl_pub = nh.advertise<rawcrcl::CrclRawCommandMsg>("rawcrcl_command", 10);
+	crcl_sub = nh.subscribe("rawcrcl_status", 1000, crclstatusCallback);
 
         spinner.stop();
         ros::spinOnce();
         do {
             myios.run_one();
+	    if( CAsioCrclSession::InMessages().SizeMsgQueue() > 0)
+		{
+                CrclMessage crclmsg = CAsioCrclSession::InMessages().PopFrontMsgQueue();
+                std::string crclcmd = boost::get<0>(crclmsg);
+                pSession = boost::get<1>(crclmsg);
+#if 0
+ 		std_msgs::String msg;
+		msg.data =crclcmd;
+ 		ROS_INFO("Crcl command: [%s]", msg.data.c_str());
+#endif
+ 		rawcrcl::CrclRawCommandMsg msg;
+		msg.xml =crclcmd;
+		msg.ip = pSession->RemoteIP();
+		msg.port = pSession->RemotePORT();
+ 		ROS_INFO("Crcl command: [%s] from %s:%d", msg.xml.c_str(), msg.ip.c_str(),msg.port);
+		crcl_pub.publish(msg);
+		}
             ros::spinOnce();
             myios.run_one();
             r.sleep();
@@ -149,7 +167,6 @@ int main(int argc, char** argv) {
     } catch (...) {
         Logger.Fatal("Abnormal exception end to  CRCL2Robot\n");
     }
-    xercesc::XMLPlatformUtils::Terminate();
     ros::shutdown();
     return 0;
 }
