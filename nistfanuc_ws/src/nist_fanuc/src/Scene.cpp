847955a2508de72e9a34e95fdc631cac8642cb5f
John L Michaloski <michalos@woodsy.el.nist.gov>
2016-08-31 16:17:03 -0400

    add save draw of scene objects

--- nistfanuc_ws/src/nist_fanuc/src/Scene.cpp
+++ nistfanuc_ws/src/nist_fanuc/src/Scene.cpp
@@ -2,15 +2,88 @@
 
 #include <rviz_visual_tools/rviz_visual_tools.h>
 #include "Globals.h"
+#include <boost/bind.hpp>
+#include <algorithm>
 using namespace rviz_visual_tools;
-
-rviz_visual_tools::RvizVisualToolsPtr visual_tools;
-visualization_msgs::Marker triangle_marker_;
+struct ObjectDB;
 
 #define XYWALL 1
 #define XZWALL 2
 #define YZWALL 3
+
 bool publishWall(int WallType, const Eigen::Affine3d & inpose, const rviz_visual_tools::colors &color, double scale);
+void DrawObject(ObjectDB *obj);
+void InitSceneObject();
+void UpdateScene(std::string objname, Eigen::Affine3d pose, rviz_visual_tools::colors color );
+void SetupSceneObject() ;
+
+struct ObjectDB
+{
+    Eigen::Affine3d pose;
+    std::string name;
+    std::string type;
+    std::string filepath;
+    rviz_visual_tools::colors color;
+    double scale;
+    std::size_t id;
+    int walltype;
+     ObjectDB(
+            std::string name,
+            std::string type,
+            std::size_t id=0,
+            Eigen::Affine3d pose=Eigen::Affine3d::Identity(),
+            std::string filepath="",
+            rviz_visual_tools::colors color=rviz_visual_tools::CLEAR,
+            double scale = 1.0) {
+        this->name = name;
+        this->pose = pose;
+        this->type = type;
+        this->filepath = filepath;
+        this->color = color;
+        this->scale = scale;
+        if(id==0)
+            this->id = gid++;
+        else this->id = id;
+     }
+
+     static void Save( ObjectDB * obj) { objects.push_back(obj); }
+
+     static ObjectDB * Find( std::size_t id) {
+        std::vector<ObjectDB *>::iterator it = std::find_if(objects.begin(), objects.end(),
+                boost::bind(&ObjectDB::id, _1) == id);
+
+        if (it != objects.end())
+            return *it;
+
+        return NULL;
+     }
+
+     static Eigen::Affine3d& FindPose(std::string name) {
+        std::vector<ObjectDB *>::iterator it = std::find_if(objects.begin(), objects.end(),
+                boost::bind(&ObjectDB::name, _1) == name);
+        if (it != objects.end())
+            return (*it)->pose;
+        return dummy->pose;
+    }
+     static ObjectDB * Find( std::string name) {
+        std::vector<ObjectDB *>::iterator it = std::find_if(objects.begin(), objects.end(), 
+        boost::bind(&ObjectDB::name, _1) == name); 
+        if(it!=objects.end())
+            return *it;
+        return NULL;
+     }
+
+   static std::size_t gid;
+   static  std::vector<ObjectDB*> objects;
+   static ObjectDB * dummy;
+};
+std::size_t ObjectDB::gid=1;
+std::vector<ObjectDB*> ObjectDB::objects;
+ObjectDB * ObjectDB::dummy= new ObjectDB("dummy","nevermatch");
+
+rviz_visual_tools::RvizVisualToolsPtr visual_tools;
+visualization_msgs::Marker triangle_marker_;
+
 
 
 void InitSceneObject() {
@@ -18,67 +91,70 @@ void InitSceneObject() {
     visual_tools->deleteAllMarkers();
 //    visual_tools->enableBatchPublishing();
     //visual_tools->waitForMarkerPub();
-    visual_tools->loadMarkerPub(true,false);
+    //visual_tools->loadMarkerPub(true,false);
     Globals.Sleep(10000);
-}
 
-// INitialize Eigen::Affine3d http://stackoverflow.com/questions/25504397/eigen-combine-rotation-and-translation-into-one-matrix
-
-void SetupSceneObject() {
-    Eigen::Affine3d pose = Eigen::Affine3d::Identity() * Eigen::Translation3d(0.5, 0, 0.0);
-    Eigen::Affine3d poseA = Eigen::Affine3d::Identity() * Eigen::Translation3d(0.25, -.45, 0.0);
-    Eigen::Affine3d pose1 = Eigen::Affine3d::Identity();
-    Eigen::Affine3d pose2 = Eigen::Affine3d::Identity() * Eigen::Translation3d(0.0, 1.0, 0.0);
-    Eigen::Affine3d pose3 = Eigen::Affine3d::Identity() * Eigen::Translation3d(0.0, -1.0, 0.0);
-    Eigen::Affine3d pose4 = Eigen::Affine3d::Identity() * Eigen::Translation3d(-1.0, 0.0, 0.0);
-#if 0
-    Eigen::Matrix3d m;
-    m = AngleAxisd(0, Vector3f::UnitZ())
-            * AngleAxisd(0, Vector3f::UnitY())
-            * AngleAxisd(0, Vector3f::UnitZ());
-
-    pose.linear() = m;
-#endif
-    double max_plane_size = 0.075;
-    double min_plane_size = 0.01;
-    bool b;
-    // b = visual_tools->publishXYPlane(pose1, rviz_visual_tools::TRANSLUCENT_DARK, 1.0);
-    //visual_tools->triggerBatchPublish();
-
-     //   b = visual_tools->publishXZPlane(pose2, rviz_visual_tools::TRANSLUCENT_LIGHT, 0.5);
-    //    b = visual_tools->publishXZPlane(pose3, rviz_visual_tools::TRANSLUCENT_LIGHT, .5);
-
-    //visual_tools->triggerBatchPublish();
-    if (!(b = visual_tools->publishMesh(pose, // or const geometry_msgs::Pose &pose
-            "file:///usr/local/michalos/nistfanuc_ws/src/nist_fanuc/worldmodel/medium_gear_holder.stl",
-            rviz_visual_tools::RED, // const colors &color = CLEAR,
-            0.035, // double scale = 1, 
-            "mesh", 
-            0))) { //  const std::size_t &id = 0))
-        std::cout << "SetupSceneObject() Failed\n";
-    }
-    visual_tools->triggerBatchPublish();
-#if 1
-    visual_tools->publishMesh(poseA, // or const geometry_msgs::Pose &pose
-            "file:///usr/local/michalos/nistfanuc_ws/src/nist_fanuc/worldmodel/medium_gear.stl",
-            rviz_visual_tools::RED, // const colors &color = CLEAR,
-            0.035, // double scale = 1, 
-            "mesh", 
-            0);
-    visual_tools->triggerBatchPublish();
-#endif*
+    // Setup up triangle_marker_ for wall drawing
     triangle_marker_.header.frame_id = "base_link";
     triangle_marker_.ns = "Triangle";
     triangle_marker_.action = visualization_msgs::Marker::ADD;
     triangle_marker_.type = visualization_msgs::Marker::TRIANGLE_LIST;
     triangle_marker_.lifetime = ros::Duration(0.0);
-    b = publishWall(XYWALL, pose1, rviz_visual_tools::TRANSLUCENT_DARK, 1);
- 
-   b = publishWall(XZWALL, pose2, rviz_visual_tools::TRANSLUCENT_DARK, 1);
-   b = publishWall(XZWALL, pose3, rviz_visual_tools::TRANSLUCENT_DARK, 1);
-   b = publishWall(YZWALL, pose4, rviz_visual_tools::TRANSLUCENT_DARK, 1);
+   
+    // Scene objects
+    ObjectDB::Save(new ObjectDB("boltholder", "mesh", ObjectDB::gid++, Eigen::Affine3d::Identity() * Eigen::Translation3d(0.5, 0, 0.0),
+            "file:///usr/local/michalos/nistfanuc_ws/src/nist_fanuc/worldmodel/medium_gear_holder.stl",
+            rviz_visual_tools::RED, 0.035));
+    ObjectDB::Save(new ObjectDB("bolt", "mesh", ObjectDB::gid++,  Eigen::Affine3d::Identity() * Eigen::Translation3d(0.25, -.45, 0.0),
+            "file:///usr/local/michalos/nistfanuc_ws/src/nist_fanuc/worldmodel/medium_gear.stl",
+            rviz_visual_tools::RED, 0.035));
+    
+    ObjectDB * obj;
+    ObjectDB::Save(obj = new ObjectDB("floor", "wall", ObjectDB::gid++, Eigen::Affine3d::Identity(),
+            "", rviz_visual_tools::TRANSLUCENT_DARK, 1.0));
+    obj->walltype = XYWALL;
+    ObjectDB::Save(obj = new ObjectDB("rightwall", "wall", ObjectDB::gid++, Eigen::Affine3d::Identity() * Eigen::Translation3d(0.0, 1.0, 0.0),
+            "", rviz_visual_tools::TRANSLUCENT_DARK, 1.0));
+    obj->walltype = XZWALL;
+        ObjectDB::Save(obj = new ObjectDB("leftwall", "wall", ObjectDB::gid++, Eigen::Affine3d::Identity() * Eigen::Translation3d(0.0, -1.0, 0.0),
+            "", rviz_visual_tools::TRANSLUCENT_DARK, 1.0));
+    obj->walltype = XZWALL;
+        ObjectDB::Save(obj = new ObjectDB("backwall", "wall", ObjectDB::gid++, Eigen::Affine3d::Identity() * Eigen::Translation3d(-1.0, 0.0, 0.0),
+            "", rviz_visual_tools::TRANSLUCENT_DARK, 1.0));
+    obj->walltype = YZWALL;
+    
 }
 
+// INitialize Eigen::Affine3d http://stackoverflow.com/questions/25504397/eigen-combine-rotation-and-translation-into-one-matrix
+
+void UpdateScene(std::string objname, Eigen::Affine3d pose, rviz_visual_tools::colors color )
+{
+    ObjectDB * obj  = ObjectDB::Find( objname);
+    obj->color=color;
+    obj->pose=pose;
+    DrawObject(obj);
+   
+}
+
+void DrawObject(ObjectDB *obj) {
+    bool b;
+    if (obj->type == "mesh")
+        visual_tools->publishMesh(obj->pose,
+            obj->filepath, // "file:///usr/local/michalos/nistfanuc_ws/src/nist_fanuc/worldmodel/medium_gear.stl",
+            obj->color, // rviz_visual_tools::RED, // const colors &color = CLEAR,
+            obj->scale, //  0.035, // double scale = 1, 
+            obj->type, // "mesh", 
+            obj->id);
+    else if (obj->type == "wall")
+        b = publishWall(obj->walltype, obj->pose, obj->color, 1);
+
+}
+void SetupSceneObject() {
+    for(size_t i=0; i< ObjectDB::objects.size(); i++){
+        ObjectDB *obj = ObjectDB::objects[i];
+        DrawObject(obj);
+    }
+}
 bool publishWall(int WallType, const Eigen::Affine3d & inpose, const rviz_visual_tools::colors &color, double scale) {
     triangle_marker_.header.stamp = ros::Time::now();
     triangle_marker_.id++;
